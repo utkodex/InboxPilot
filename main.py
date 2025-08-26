86d2e9febbc27bad2dfc490b327108d87cf03b06
utkodex <bizzboosterdata@gmail.com>
2025-08-26 17:19:59 +0530

    added cc and bcc codes

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from email_auto.email_sender import EmailSender
+from google_sheet.google_sheet import GoogleSheetClient
 
 
-a=EmailSender()
-recipient_email= "bizzboosterdata@gmail.com" 
-subject= "Subject check" 
-body= "This is a test email from the EmailSender2 class."
+def send_email():
+    email_sender=EmailSender()
+    recipient_email= "bizzboosterdata@gmail.com" 
+    subject= "Subject check" 
+    body= "This is a test email from the EmailSender2 class."
 
-a.send_email(recipient_email, subject, body)
+    email_sender.send_email(recipient_email, subject, body)
+
+a=GoogleSheetClient().dataset_to_json()
+# logging.info(a["Recipient Email"])
+
+for _ in a:
+    print(f'ewp: {_["Recipient Email"]}')
